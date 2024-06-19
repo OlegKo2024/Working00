653327e278bc1c40c36b2724f549be9ae4a479d7
OlegKo2024 <4oki66@gmail.com>
2024-06-19 19:28:50 +0300

    Add files via upload

---
+++ PLesson00.py
@@ -0,0 +1,34 @@
+print("19.06.2024")
+print('Задача «Длина слова»')
+a = 5
+print(len('a'))
+
+print('Задача «Суммы и разности»')
+first = 3
+second = 5
+summa = first + second
+print(summa)
+diff = first - second
+print(diff)
+
+print('Задача «Среднее арифметическое»')
+first = 4
+second = 6
+third = 8
+summa = first + second + third
+list = ('first', 'second', 'third')
+count = len(list)
+mean = (summa // count)
+print('Средрее арифметическое', '=', mean)
+
+print('Задача «Простые строчки»')
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string + ', ' + first_string)
+
+print('Задача «Сложная формула»')
+a = 4
+b = 6
+c = 8
+f = (a * b) + (a * c)
+print(f)
